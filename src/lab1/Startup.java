28eec526025848185690a41759e3f1260d15c0cf
mjmersenski <mjmersenski@fe80:0:0:0:d341:a620:5df7:97b7%10>
2017-09-14 15:06:58 -0500

    added comments to startup

--- src/lab1/Startup.java
+++ src/lab1/Startup.java
@@ -7,5 +7,14 @@ package lab1;
 public class Startup {
     public static void main(String[] args) {
         
+        
+        /**
+         * Why do I need the subclasses for IntroToProgramming, IntroJavaCourse, and AdvancedJavaCourse?
+         * Could I just use my SchoolCourse and SchoolCourseWithPrerequisites classes to create
+         * instances for each one?
+         * 
+         * I guess the only reason I would want to keep them around is incase I need to add properties
+         * or methods specific to the classes.
+         */
     }
 }
