90bc666d546c8f0dd7c03f29d1a80813e09fa863
mjmersenski <mjmersenski@fe80:0:0:0:d341:a620:5df7:97b7%10>
2017-09-14 14:51:28 -0500

    added a superclass for subclasses with prerequisites

---
+++ src/lab1/SchoolCourseWithPrerequisites.java
@@ -0,0 +1,28 @@
+package lab1;
+
+import javax.swing.JOptionPane;
+
+/**
+ * This class is being added for subclasses
+ * that have prerequisites, and all the
+ * properties of the superclass, SchoolCourse
+ *
+ * @author      mmersenski
+ * @version     1.00
+ */
+public class SchoolCourseWithPrerequisites {
+    private String prerequisites;
+    
+        public String getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setPrerequisites(String prerequisites) {
+        if(prerequisites == null || prerequisites.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: prerequisites cannot be null of empty string");
+            System.exit(0);
+        }
+        this.prerequisites = prerequisites;
+    }
+}
