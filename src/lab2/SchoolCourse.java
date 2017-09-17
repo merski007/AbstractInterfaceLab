c8ab35a1fbd79abc7c12ecb092a0ca1c7f050c19
mjmersenski <mjmersenski@fe80:0:0:0:d341:a620:5df7:97b7%10>
2017-09-17 08:51:15 -0500

    created the interface in lab 2 with all necessary methods

---
+++ src/lab2/SchoolCourse.java
@@ -0,0 +1,17 @@
+package lab2;
+
+public interface SchoolCourse {
+    public void setCourseInfo(String courseName, String courseNumber);
+
+    public String getCourseName();
+
+    public void setCourseName(String courseName);
+
+    public String getCourseNumber();
+
+    public void setCourseNumber(String courseNumber);
+
+    public double getCredits();
+
+    public void setCredits(double credits);
+}
