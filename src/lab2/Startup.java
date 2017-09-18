112b3b5e80562aa895586a00c6bb745b9c4c9c72
mjmersenski <mjmersenski@fe80:0:0:0:d341:a620:5df7:97b7%10>
2017-09-18 09:06:45 -0500

    updated java classes to blank shells and created the startup class for testing

--- src/lab2/Startup.java
+++ src/lab2/Startup.java
@@ -2,6 +2,32 @@ package lab2;
 
 public class Startup {
     public static void main(String[] args) {
+        SchoolCourse c1 = new IntroToProgrammingCourse();
+        SchoolCourse c2 = new IntroJavaCourse();
+        SchoolCourse c3 = new AdvancedJavaCourse();
         
+        c1.setCourseInfo("Intro To Programming", "101-125");
+        c2.setCourseInfo("Intro To Java", "152-131");
+        c3.setCourseInfo("Advanced Java", "152-150");
+        
+        SchoolCourseWithPrerequisites pr1 = new IntroJavaCourse();
+        SchoolCourseWithPrerequisites pr2 = new AdvancedJavaCourse();
+        
+        pr1.setPrerequisites("Intro To Programming");
+        pr2.setPrerequisites("Intro to Java");
+        
+        /**
+         * For this instance, and with my design, an interface is a bad choice.
+         * The reason it is a bad choice is because every instance of a regular
+         * SchoolCourse would require implementation of the interface. This has
+         * the potential to be a ton of work if any changes were made, thus making
+         * the code very rigid.
+
+         * 
+         * The Liskov Substitution principle helps make the above code very flexible.
+         * For example, perhaps the intro java course no longer has a prerequisites course.
+         * The code variable pr1 could quickly be created as a SchooCourse object
+         * without having to change any other code.
+         */
     }
 }
