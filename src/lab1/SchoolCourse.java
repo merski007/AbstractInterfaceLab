4287ac4f0e7f0eb64b94852ec4378fb346685557
mjmersenski <mjmersenski@fe80:0:0:0:d341:a620:5df7:97b7%10>
2017-09-18 20:52:50 -0500

    removed prerequisites class and set all methods to final in SchoolCourse

--- src/lab1/SchoolCourse.java
+++ src/lab1/SchoolCourse.java
@@ -13,16 +13,16 @@ public abstract class SchoolCourse {
     private double credits;
     private String prerequisites;
     
-    public void setCourseInfo(String courseName, String courseNumber) {
+    public final void setCourseInfo(String courseName, String courseNumber) {
         setCourseName(courseName);
         setCourseNumber(courseNumber);
     }
     
-    public String getPrerequisites() {
+    public final String getPrerequisites() {
         return prerequisites;
     }
 
-    public void setPrerequisites(String prerequisites) {
+    public final void setPrerequisites(String prerequisites) {
         if(prerequisites == null || prerequisites.length() == 0) {
             JOptionPane.showMessageDialog(null,
                     "Error: prerequisites cannot be null of empty string");
@@ -31,11 +31,11 @@ public abstract class SchoolCourse {
         this.prerequisites = prerequisites;
     }
 
-    public String getCourseName() {
+    public final String getCourseName() {
         return courseName;
     }
 
-    public void setCourseName(String courseName) {
+    public final void setCourseName(String courseName) {
         if (courseName == null || courseName.length() == 0) {
             JOptionPane.showMessageDialog(null,
                     "Error: courseName cannot be null of empty string");
@@ -44,11 +44,11 @@ public abstract class SchoolCourse {
         this.courseName = courseName;
     }
 
-    public String getCourseNumber() {
+    public final String getCourseNumber() {
         return courseNumber;
     }
 
-    public void setCourseNumber(String courseNumber) {
+    public final void setCourseNumber(String courseNumber) {
         if (courseNumber == null || courseNumber.length() == 0) {
             JOptionPane.showMessageDialog(null,
                     "Error: courseNumber cannot be null of empty string");
@@ -57,11 +57,11 @@ public abstract class SchoolCourse {
         this.courseNumber = courseNumber;
     }
 
-    public double getCredits() {
+    public final double getCredits() {
         return credits;
     }
 
-    public void setCredits(double credits) {
+    public final void setCredits(double credits) {
         if (credits < 0.5 || credits > 4.0) {
             JOptionPane.showMessageDialog(null,
                     "Error: credits must be in the range 0.5 to 4.0");
